d2476038afc75748c3bebae092238a352cae0b6a
connor135246 <66351014+connor135246@users.noreply.github.com>
2020-06-03 07:06:26 -0400

    v1.3

--- src/main/java/connor135246/campfirebackport/common/CommonProxy.java
+++ src/main/java/connor135246/campfirebackport/common/CommonProxy.java
@@ -1,6 +1,7 @@
 package connor135246.campfirebackport.common;
 
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Random;
 
 import org.apache.logging.log4j.Logger;
@@ -8,6 +9,8 @@ import org.apache.logging.log4j.Logger;
 import connor135246.campfirebackport.CampfireBackport;
 import connor135246.campfirebackport.CampfireBackportConfig;
 import connor135246.campfirebackport.common.blocks.CampfireBackportBlocks;
+import connor135246.campfirebackport.common.dispenser.BehaviourShovel;
+import connor135246.campfirebackport.common.dispenser.BehaviourSword;
 import connor135246.campfirebackport.common.tileentity.TileEntityCampfire;
 import connor135246.campfirebackport.util.CampfireBackportEventHandler;
 import connor135246.campfirebackport.util.Reference;
@@ -15,11 +18,20 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.FMLControlledNamespacedRegistry;
+import cpw.mods.fml.common.registry.GameData;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockDispenser;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemSpade;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
@@ -33,7 +45,7 @@ public class CommonProxy
     public static CampfireBackportEventHandler handler = new CampfireBackportEventHandler();
 
     public static Logger modlog;
-    
+
     public void preInit(FMLPreInitializationEvent event)
     {
         config = new Configuration(event.getSuggestedConfigurationFile());
@@ -55,6 +67,9 @@ public class CommonProxy
     public void postInit(FMLPostInitializationEvent event)
     {
         syncConfig();
+
+        if (CampfireBackportConfig.dispenserBehaviours)
+            registerShovelsAndSwordsInDispenser();
     }
 
     public static void syncConfig()
@@ -74,8 +89,90 @@ public class CommonProxy
         }
     }
 
+    @SuppressWarnings("null")
+    public static void registerShovelsAndSwordsInDispenser()
+    {
+        ArrayList<Item> bitemlist = CampfireBackportConfig.dispenserBehavioursBlacklistItems;
+        boolean print = CampfireBackportConfig.printDispenserBehaviours;
+        FMLControlledNamespacedRegistry<Item> itemreg = GameData.getItemRegistry();
+        FMLControlledNamespacedRegistry<Block> blockreg = GameData.getBlockRegistry();
+
+        iteratorLoop: for (Item item : itemreg.typeSafeIterable())
+        {
+            if (item instanceof ItemSpade)
+            {
+                for (Item bitem : bitemlist)
+                {
+                    if (item == bitem)
+                        continue iteratorLoop;
+                }
+                BlockDispenser.dispenseBehaviorRegistry.putObject(item, new BehaviourShovel());
+
+                if (print)
+                    modlog.info("Dispenser Behaviour (Shovel) added to: " + item.getItemStackDisplayName(new ItemStack(item)));
+
+            }
+            else if (item instanceof ItemSword)
+            {
+                for (Item bitem : bitemlist)
+                {
+                    if (item == bitem)
+                        continue iteratorLoop;
+                }
+                BlockDispenser.dispenseBehaviorRegistry.putObject(item, new BehaviourSword());
+
+                if (print)
+                    modlog.info("Dispenser Behaviour (Sword)  added to: " + item.getItemStackDisplayName(new ItemStack(item)));
+
+            }
+        }
+        
+        for (String witem : CampfireBackportConfig.dispenserBehavioursWhitelist)
+        {
+            String[] segment = witem.split("/");
+            Item item = itemreg.getObject(segment[0]);
+
+            if (item != null)
+            {
+                if (segment[1].equals("shovel"))
+                {
+                    BlockDispenser.dispenseBehaviorRegistry.putObject(item, new BehaviourShovel());
+                    if (print)
+                        modlog.info("Dispenser Behaviour (Shovel) added to: " + item.getItemStackDisplayName(new ItemStack(item)));
+                }
+                else //if (segment[1].equals("sword"))
+                {
+                    BlockDispenser.dispenseBehaviorRegistry.putObject(item, new BehaviourSword());
+                    if (print)
+                        modlog.info("Dispenser Behaviour (Sword)  added to: " + item.getItemStackDisplayName(new ItemStack(item)));
+                }
+                continue;
+            }
+            
+            Block block = blockreg.getObject(segment[0]);
+
+            if (block != Blocks.air)
+            {
+                if (segment[1].equals("shovel"))
+                {
+                    BlockDispenser.dispenseBehaviorRegistry.putObject(block, new BehaviourShovel());
+                    if (print)
+                        modlog.info("Dispenser Behaviour (Shovel) added to: " + item.getItemStackDisplayName(new ItemStack(block)));
+                }
+                else //if (segment[1].equals("sword"))
+                {
+                    BlockDispenser.dispenseBehaviorRegistry.putObject(block, new BehaviourSword());
+                    if (print)
+                        modlog.info("Dispenser Behaviour (Sword)  added to: " + item.getItemStackDisplayName(new ItemStack(block)));
+                }
+            }
+        }
+        
+    }
+
     public void generateBigSmokeParticles(World world, int x, int y, int z, boolean signalFire)
     {
         ;
     }
+    
 }
